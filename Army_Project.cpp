dc9b9a279aefca2d50735b41edf983743b16d2a6
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-07-12 20:01:01 +0300

    Добавьте файлы проекта.

---
+++ Army_Project.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+class Weapon
+{
+public:
+	virtual void func_1() = 0;
+	virtual void func_2() = 0;
+};
+
+class C_Child : public Weapon
+{
+public:
+	virtual void func_1() override { std::cout << "func 1 of child\n"; }
+	virtual void func_2() override { std::cout << "func 2 of child\n"; }
+};
