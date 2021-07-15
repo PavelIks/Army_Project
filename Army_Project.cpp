f51b6e3e21268cd6b33057766c4c1be2f955fe6d
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-07-15 10:29:34 +0300

    Updated-1.

--- Army_Project.cpp
+++ Army_Project.cpp
@@ -1,15 +1,78 @@
 ﻿#include <iostream>
 
+class Armour
+{
+public:
+	virtual void lomatsa() = 0;
+	virtual void prinat_udar(int moshnost_udara) = 0;
+	virtual void getHP() = 0;
+private:
+	int protection; // прочность
+	int weight; // вес
+};
+
+class Ligh_armor : public Armour
+{
+
+};
+
+class Heavy_armor : public Armour
+{
+
+};
+
+class Leather_armor : public Ligh_armor
+{
+
+};
+
+class Chain_mail : public Ligh_armor
+{
+
+};
+
+class Plate_armor : public Heavy_armor
+{
+
+};
+
 class Weapon
 {
 public:
-	virtual void func_1() = 0;
-	virtual void func_2() = 0;
+	virtual void attack() = 0;
+	virtual void broke() = 0;
 };
 
-class C_Child : public Weapon
+class Hand_Weapon : public Weapon
 {
 public:
-	virtual void func_1() override { std::cout << "func 1 of child\n"; }
-	virtual void func_2() override { std::cout << "func 2 of child\n"; }
+	virtual void attack() override { std::cout << "Оружие, атака\n"; }
+	virtual void broke() override { std::cout << "Оружие, ломаца\n"; }
+	virtual void protection() = 0;
 };
+#pragma region Hand_Weapon: Sword & Saber
+class Sword final : public  Hand_Weapon
+{
+public:
+	virtual void protection() override { std::cout << "Оружие, защита\n"; }
+};
+class Saber final : public Hand_Weapon
+{
+public:
+	virtual void protection() override { std::cout << "Оружие, защита\n"; }
+};
+#pragma endregion
+
+class Ranged_Weapon : public Weapon
+{
+public:
+	virtual void charging() = 0;
+};
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	//Weapon* obj = new Hand_Weapon();
+	//obj->attack();
+	//obj->broke();
+}
