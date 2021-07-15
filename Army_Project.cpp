f700e0fc90ff1b5a23f5aec4afb82c4b2d11e112
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-07-15 14:05:11 +0300

    Updated-2.

--- Army_Project.cpp
+++ Army_Project.cpp
@@ -11,63 +11,88 @@ private:
 	int weight; // вес
 };
 
-class Ligh_armor : public Armour
+class Ligh_armor : public Armour // Лёгкая броня
 {
 
 };
 
-class Heavy_armor : public Armour
+class Heavy_armor : public Armour // Тяжёлая броня
 {
 
 };
 
-class Leather_armor : public Ligh_armor
+class Leather_armor : public Ligh_armor // Коженная броня
 {
 
 };
 
-class Chain_mail : public Ligh_armor
+class Chain_mail : public Ligh_armor // Колчуга
 {
 
 };
 
-class Plate_armor : public Heavy_armor
+class Plate_armor : public Heavy_armor // Латная броня
 {
 
 };
 
-class Weapon
+class Weapon // Оружие
 {
 public:
 	virtual void attack() = 0;
 	virtual void broke() = 0;
 };
 
-class Hand_Weapon : public Weapon
+class Hand_Weapon : public Weapon // Ручное оружие
 {
 public:
 	virtual void attack() override { std::cout << "Оружие, атака\n"; }
 	virtual void broke() override { std::cout << "Оружие, ломаца\n"; }
 	virtual void protection() = 0;
 };
-#pragma region Hand_Weapon: Sword & Saber
-class Sword final : public  Hand_Weapon
+class Sword final : public  Hand_Weapon // Мечь
 {
 public:
 	virtual void protection() override { std::cout << "Оружие, защита\n"; }
 };
-class Saber final : public Hand_Weapon
+class Saber final : public Hand_Weapon // Сабля
 {
 public:
 	virtual void protection() override { std::cout << "Оружие, защита\n"; }
 };
-#pragma endregion
 
-class Ranged_Weapon : public Weapon
+class Ranged_Weapon : public Weapon // Оружие стрелковое
 {
 public:
 	virtual void charging() = 0;
 };
+class Onion : public Ranged_Weapon // Лук
+{
+public:
+	virtual void charging() override { std::cout << "Оружие, перезарядка\n"; }
+};
+class Crossbow final : public Onion // Арбалет
+{
+public:
+	virtual void charging() override { std::cout << "Оружие, перезарядка\n"; }
+};
+
+class Polearms : public Weapon // Древковое оружие
+{
+public:
+	virtual void burn() = 0;
+};
+class Spear : public Polearms // Копьё
+{
+public:
+	virtual void burn() override { std::cout << "Оружие, гореть\n"; }
+};
+class Halberd final : public Spear // Алебарда
+{
+public:
+	virtual void burn() override { std::cout << "Оружие, гореть\n"; }
+};
+
 
 int main()
 {
